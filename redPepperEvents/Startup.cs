e79526a1213214ce12fc2e19d9b8c83dcf1416d2
Zaheera Kamaar <ZKamaar@jhb.dvt.co.za>
2016-09-26 15:36:28 +0200

    Added new project

---
+++ redPepperEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(redPepperEvents.Startup))]
+namespace redPepperEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

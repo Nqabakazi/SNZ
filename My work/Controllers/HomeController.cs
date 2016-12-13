826e1cdd9005beaec0cbe1d3b9081e6aab139412
Sindisiwe Mashini <SMashini@jhb.dvt.co.za>
2016-12-13 10:58:22 +0200

    Shopping cart modified

--- My work/Controllers/HomeController.cs
+++ My work/Controllers/HomeController.cs
@@ -10,6 +10,9 @@ namespace My_work.Controllers
     public class HomeController : Controller
     {
         public static ProductDetails productInfo = new ProductDetails();
+        static CartItems Cart = new CartItems();
+        
+    
 
         // GET: Home
         public ActionResult Index()
@@ -38,18 +41,88 @@ namespace My_work.Controllers
             return View(productInfo.getProInfo(ItemNum));
         }
 
-
-
         public ActionResult ProductDetails()
         {
 
             productInfo.ProInfo();
             return View(productInfo.pro);
 
-           
+
+        }
+        public ActionResult CartItems()
+        {
+            List<CartDetails> myList = Session["prof"] as List<CartDetails>;
+            return View(myList);
+        }
+        public ActionResult CartDetails()
+        {
+            return View();
         }
 
 
+        public ActionResult AddToCart(string ItemNum)
+        {
+            //psudocode
+            
 
+            if (Session["Prof"] == null)
+            {
+                Session["prof"] = Cart;
+            }
+
+            //Check if item is already in cart
+            bool found = false;
+            int indexOfFound = -1;
+            for (int i = 0; i < Cart.getItems().Count; i++)
+            {
+                CartDetails p = Cart.getItems()[i];
+                if (p.ItemNumber.Equals(ItemNum, StringComparison.Ordinal))
+                {
+                    found = true;
+                    indexOfFound = i;
+                    break;
+                }
+            }
+
+            //if not, add to card
+            //Else increase quantity by ones
+            if (found)
+            {
+                Cart.incrementQuantity(indexOfFound);
+                //Cart.getTotal();
+
+            } else {
+                CartDetails product = new CartDetails();
+
+                Products item = productInfo.getProInfo(ItemNum);
+                product.ItemNumber = item.ItemNumber;
+                product.productName = item.ProductName;
+                product.Description = item.ProductDiscription;
+                product.Quantity = 1;
+                product.subTotal = item.ProPrice;
+                product.total = Cart.getTotal();
+                    
+              
+                
+                Cart.addProduct(product);
+                ViewBag.MyCarts = Cart.catDetails.Count();
+                Session["prof"] = Cart.catDetails;
+            }
+            
+            
+            var list = Session["cart"] as CartItems;
+            return View("Product");
+        }
+     
+        public ActionResult Delete(string ItemNumber)
+        {
+            List<CartDetails> listo = Session["prof"] as List<CartDetails>;
+            var item = listo.SingleOrDefault(x => x.ItemNumber == ItemNumber);
+            if (item != null)
+                listo.Remove(item);
+            return RedirectToAction("CartItems", "Home");
+        }
+
+        }
     }
-}
+
--- My work/Models/ProductDetails.cs
+++ My work/Models/ProductDetails.cs
@@ -21,50 +21,50 @@ namespace My_work.Models
         {
             pro.Clear();
             // pic1
-           
-            pr1.ProductName = "Food";
-            pr1.ProductDiscription = "";
+
+            pr1.ProductName = "Weeding decor 2";
+            pr1.ProductDiscription = "life worth weeding decor a bride could wish for";
             pr1.ProPrice = 10000.00;
             pr1.ItemNumber = "125516";
             pro.Add(pr1);
 
             // pic2
             
-            pr2.ProductName = "Food";
-            pr2.ProductDiscription = "";
-            pr2.ProPrice = 10000.00;
+            pr2.ProductName = "Cherish weeding decor";
+            pr2.ProductDiscription = "a weeding decor with style ";
+            pr2.ProPrice = 12000.00;
             pr2.ItemNumber = "1255112";
             pro.Add(pr2);
 
             // pic3
-            
-            pr3.ProductName = "Food";
-            pr3.ProductDiscription = "";
-            pr3.ProPrice = 10000.00;
+
+            pr3.ProductName = "Weeding decor 2";
+            pr3.ProductDiscription = " romantic-design-luxury-wedd and white weeding decoration ";
+            pr3.ProPrice = 18000.00;
             pr3.ItemNumber = "125517";
             pro.Add(pr3);
 
             // pic4
-            
-            pr4.ProductName = "Food";
-            pr4.ProductDiscription = "";
-            pr4.ProPrice = 10000.00;
+
+            pr4.ProductName = "Party event decor 2";
+            pr4.ProductDiscription = "part event for afte parties and formal parties ";
+            pr4.ProPrice = 18000.00;
             pr4.ItemNumber = "125518";
             pro.Add(pr4);
 
             // pic5
-            
-            pr5.ProductName = "Food";
-            pr5.ProductDiscription = "";
-            pr5.ProPrice = 10000.00;
-            pr5.ItemNumber = "1255119";
+
+            pr5.ProductName = "Party event decor 1";
+            pr5.ProductDiscription = "part event for afte parties or graduations";
+            pr5.ProPrice = 16000.00;
+            pr5.ItemNumber = "1255119"; 
             pro.Add(pr5);
 
             // pic6
-            
-            pr6.ProductName = "Food";
-            pr6.ProductDiscription = "";
-            pr6.ProPrice = 10000.00;
+
+            pr6.ProductName= "Wedding Decor Classic";
+            pr6.ProductDiscription = " weeding decor with an extra suprise for the bride";
+            pr6.ProPrice = 13000.00;
             pr6.ItemNumber = "1255110";
             pro.Add(pr6);
 
@@ -72,6 +72,8 @@ namespace My_work.Models
         public Products getProInfo(string ProductName)
 
         {
+            ProInfo();
+
             Products filter = null;
             foreach (Products item in pro)
             {
